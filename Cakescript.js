893399c0363675bf708b5b9c16cba876b0f5bcca
Loysel <109748106+Loysel-Cakes@users.noreply.github.com>
2022-07-24 16:58:00 +0200

    m

--- Cakescript.js
+++ Cakescript.js
@@ -1,52 +1,46 @@
 
 var isOpen = false;
-
-
-function OpenNav(){
-    //var closeBtn = document.getElementById("closeBtn");   
-    if(!isOpen){           
-     var Snav = document.getElementById("SideNav");  
-     var OpenBtn = document.getElementById("Menu");
-     var contactSection = document.getElementById("ContactSection");
-     var cakeSection = document.getElementById("CakeSection");
-     var cupcakeSection = document.getElementById("CupCakeSection");
-     var macaronSection = document.getElementById("MacaronSection");
-     var donutSection = document.getElementById("DonutSection");
-      cakeSection.style.visibility = "visible";
-     donutSection.style.visibility = "visible";
-     macaronSection.style.visibility = "visible";
-     cupcakeSection.style.visibility = "visible";
-    //OpenBtn.style.visibility = "visible";  
-    contactSection.style.visibility = "hidden";
-    Snav.style.width = "250px";
-    Snav.style.height = "100%";
-    Snav.style.top = "70px";
-    isOpen = true;
-    } 
-
- 
-   }
-   function CloseNav(){
-    if(isOpen){
-   var closeBtn = document.getElementById("Menu");  
+var isClicked = false;
+   var MenuBtn = document.getElementById("Menu");  
     var Snav = document.getElementById("SideNav");
-    //var OpenBtn = document.getElementById("closeBtn"); 
     var contactSection= document.getElementById("ContactSection");
     var cakeSection = document.getElementById("CakeSection");
     var cupcakeSection = document.getElementById("CupCakeSection");
     var macaronSection = document.getElementById("MacaronSection");
     var donutSection = document.getElementById("DonutSection");
+
+function MenuBtnPressed(){
+      
+    isClicked = true;
+   }
+    if(isClicked){ 
+      if(!isOpen){
+        OpenNav();
+      }           
+      if(isOpen){
+        CloseNav();
+      }
+    }
+    function OpenNav(){
+    
+    cakeSection.style.visibility = "visible";
+     donutSection.style.visibility = "visible";
+     macaronSection.style.visibility = "visible";
+     cupcakeSection.style.visibility = "visible";
+    contactSection.style.visibility = "hidden";
+    Snav.style.width = "250px";
+    Snav.style.height = "100%";
+    Snav.style.top = "70px";
+    isOpen = true;
+    }
+   function CloseNav(){ 
      cakeSection.style.visibility = "hidden";
     donutSection.style.visibility = "hidden";
     macaronSection.style.visibility = "hidden";
     cupcakeSection.style.visibility = "hidden"; 
     contactSection.style.visibility = "visible";
-    //OpenBtn.style.visibility = "hidden";
-    //closeBtn.hidden = false;
      Snav.style.width = "60px";
      Snav.style.top = "0px";
     Snav.style.height = "60px"; 
     isOpen = false; 
     }
- 
-  }
